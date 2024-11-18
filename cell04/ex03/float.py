919eaf15e55cec97e46e09af3c72c8c25253191d
ritepsam <ritepsam>
2024-11-18 22:08:59 +0700

    lastest

---
+++ cell04/ex03/float.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+number = input("Give me a number: ")
+A = number.isdigit()
+if A == True:
+    print("This number is an integer.")
+else:
+    print("This number is an decimal.")
+    
---
+++ cell04/ex04/round.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import math
+number = float(input("Give me a number: "))
+print(int(math.ceil(number)))
