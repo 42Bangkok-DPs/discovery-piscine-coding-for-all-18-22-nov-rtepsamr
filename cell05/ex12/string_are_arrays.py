8f30e9205e3cf5b0b61a8c510e9019f09fa24c7a
ritepsam <ritepsam>
2024-11-19 20:46:31 +0700

    .

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+item_list = sys.argv
+item_list.pop(0)
+i = len(sys.argv)
+if i == 0:
+    print("none")
+else:
+    sentence = sys.argv[0]
+    count_z = sentence.count("z")
+    print("z"*count_z)
---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+first_number = int(sys.argv[1])
+second_number = int(sys.argv[2])
+x = range((first_number),(second_number)+1)
+array = list(x)
+print(array)
