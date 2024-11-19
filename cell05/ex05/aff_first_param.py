1b674682472fcf84109d7a0d566b5718784f711d
ritepsam <ritepsam>
2024-11-19 17:31:22 +0700

    .

--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 import sys
-item_list = sys.argv
-item_list.pop(0)
-i = len(item_list)
-if i <= 2:
+if len(sys.argv) == 1:
     print("none")
 else:
-    reversed_list = item_list[::-1]
-for x in range(i):
-    print(reversed_list[x])
+    print(sys.argv[1])
