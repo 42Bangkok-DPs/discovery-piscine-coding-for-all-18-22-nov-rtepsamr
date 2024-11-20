8be30086fd87deec22c9989bc44b7502803c5f8e
ritepsam <ritepsam>
2024-11-20 10:50:40 +0700

    .

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-number = input("Give me a number: ")
-A = number.isdigit()
-if A == True:
+number = float(input("Give me a number: ",))
+if number.is_integer() == True:
     print("This number is an integer.")
 else:
     print("This number is an decimal.")
