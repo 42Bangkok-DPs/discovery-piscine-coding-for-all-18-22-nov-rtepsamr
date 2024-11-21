22d0c413e76d9d39c756c8c8c01135d65a5586cc
Rittigrai Tepsamrithporn <lucky.rtp@Luckys-Mac-mini.local>
2024-11-21 16:18:25 +0700

    .

--- cell07/ex03/persons_of_interest.py
+++ cell07/ex03/persons_of_interest.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 def famous_births(scientists_data):
-    export_data = [f"{name} is a great scientist born in {date_of_birth}" for name, date_of_birth in women_scientists.item()]
-    return print(export_data)
+
+    sorted_scientists = sorted(
+        scientists_data.values(), key=lambda x: x["date_of_birth"]
+    )
+    export_data = [
+        f"{scientist['name']} is a great scientist born in {scientist['date_of_birth']}"
+        for scientist in sorted_scientists
+    ]
+    for line in export_data:
+        print(line)
+
 women_scientists = {
-"ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
-"cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
-"lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
-"grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+    "ada": {"name": "Ada Lovelace", "date_of_birth": "1815"},
+    "cecilia": {"name": "Cecila Payne", "date_of_birth": "1900"},
+    "lise": {"name": "Lise Meitner", "date_of_birth": "1878"},
+    "grace": {"name": "Grace Hopper", "date_of_birth": "1906"},
 }
+
 famous_births(women_scientists)
